70f25702ff61c2b7b71beb608268f7fd6e54b30c
Haziq Rozaidi <193243546+mhaziqrozaidi@users.noreply.github.com>
2025-05-18 21:35:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quest_list
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/quest_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quest_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
